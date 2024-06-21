3fb40bceac7f130f29e7104206003f87f30cb2d2
Alexander Close <ardclose@gmail.com>
2024-06-21 14:41:17 +0200

    Updates data_extr file

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,11 @@
-Class DataExtractor:
-	
+from database_utils.py import DatabaseConnector as DbC
+
+class DataExtractor:
+  def __init__(self,yaml_file):
+    self.yaml_file=yaml_file
+  def read_rds_table(self):
+    connector = DbC(self.yaml_file)
+    return 'worked'
+
+inst = DataExtractor('db_creds.yaml')
+inst.reads_rdstable()
