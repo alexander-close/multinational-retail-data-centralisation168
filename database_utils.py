c187bb8067a6b4380a7d4bfb606808bfa0aa3994
Alexander Close <ardclose@gmail.com>
2024-06-21 14:22:11 +0200

    Adds init_db_engine method to database_utils.py

--- database_utils.py
+++ database_utils.py
@@ -1,5 +1,5 @@
 import yaml
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect, MetaData,text
 
 yaml_file = 'db_creds.yaml'
 
@@ -14,18 +14,60 @@ class DatabaseConnector:
   def read_db_creds(self):
     with open(self.file_path,'r') as file:
       return yaml.safe_load(file)
+    
   def init_db_engine(self):
-    instance = DatabaseConnector(yaml_file)
-    creds = instance.read_db_creds()
-    conn_str = f"postgresql://{creds['RDS_USER']}:\
-    {creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:\
-    {creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
-    engine = create_engine(conn_str)
-    return engine.connect()
+    creds = self.read_db_creds()
+    conn_str = f"postgresql://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
+    return create_engine(conn_str)
+    
+  def list_db_tables(self,schema='public'):
+    engine = self.init_db_engine()
+    with engine.connect() as conn:
+        result = conn.execute(f"select count(*) from legacy_users")
+        tables = [row[0] for row in result]
+    return tables
+    # inspector = inspect(engine)
+    # return inspector.get_table_names(schema=schema)
 
-
-# init_db_engine = psycopg2.connect(host=HOST, user=USER, password=PASSWORD, dbname=DBNAME, port=PORT)
+  def list_tables(self, schema='public'):
+    engine = self.init_db_engine()
+    metadata = MetaData()
+    metadata.reflect(engine, schema=schema)
+    return list(metadata.tables.keys())
 
 if __name__ == '__main__':
   inst = DatabaseConnector(yaml_file)
-  inst.init_db_engine()
+
+  engine = inst.init_db_engine()
+  with engine.connect() as conn:
+    result = conn.execute(f"select count(*) from legacy_users")
+    tables = [row[0] for row in result]
+    print(tables)
+  # print(inst.list_db_tables())
+  # engine = inst.init_db_engine()
+
+  # inspector = inspect(engine)
+  # print(inspector.get_table_names())
+
+  # with engine.connect() as connection:
+  #   result = connection.execute(text("SELECT * FROM information_schema.tables"))
+    # for row in result:
+    #   print(row)
+  # print(tab)
+
+# from sqlalchemy import create_engine,text
+
+# DATABASE_TYPE = 'postgresql'
+# DBAPI = 'psycopg2'
+# HOST = 'data-handling-project-readonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com'
+# USER = 'aicore_admin'
+# PASSWORD = 'AiCore2022'
+# DATABASE = 'postgres'
+# PORT = 5432
+
+# engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+
+# with engine.connect() as connection:
+#     result = connection.execute(text("select * from information_schema.tables"))
+#     for row in result:
+#         print(row)
