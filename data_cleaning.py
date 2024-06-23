dacf6f7e0632c9bc4ebefcd0403807401354d5fb
Alexander Close <ardclose@gmail.com>
2024-06-23 13:44:57 +0200

    Adds clean_card_data method to DataCleaning class

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,4 +2,8 @@ class DataCleaning:
   def __init__(self) -> None:
     pass
   def clean_user_data(self):
+    # needs to fix NULL values, errors with dates, incorrectly typed values and rows filled with the wrong information
     pass
+  def clean_card_data(self):
+    pass
+    # cleans data to remove any erroneous values, NULL values or errors with formatting
