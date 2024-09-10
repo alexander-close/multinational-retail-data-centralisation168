3dc9e42dc8c358078972ce73851fce9207e943d7
Alexander Close <ardclose@gmail.com>
2024-09-10 14:09:43 +0100

    Adds type hints. Removes unneeded connection closer.

--- data_extraction.py
+++ data_extraction.py
@@ -6,71 +6,67 @@ import yaml
 from io import BytesIO
 from database_utils import DatabaseConnector
 
-with open('constants.yaml','r') as f:
+with open('constants.yaml', 'r') as f:
   consts = yaml.safe_load(f)
 PDF_URL = consts['PDF_URL']
 ENDPOINT = consts['ENDPOINT']
-# NOS_URL = consts['STORE_URL'].format('number_stores')
-# STORE_URL = consts['STORE_URL'].format('store_details/{store_number}')
-with open('keys.yaml','r') as g:
+
+with open('keys.yaml', 'r') as g:
   keys = yaml.safe_load(g)
 headers = {'x-api-key':keys['x-api-key']}
 
 class DataExtractor:
   def __init__(self):
-    self.instance = DatabaseConnector()
-    self.number_stores = 0
+    self.instance: object = DatabaseConnector()
+    self.number_stores: int = 0
 
-  def read_rds_table(self,table_name:str):
+  def read_rds_table(self, table_name:str) -> pd.DataFrame | str:
     engine = self.instance.init_db_engine()
     if table_name in self.instance.list_db_tables():
       return pd.read_sql_table(table_name,engine)
     else:
       return 'Table not in schema.'
     
-  def retrieve_pdf_data(self,url:str=PDF_URL):
+  def retrieve_pdf_data(self, url:str=PDF_URL):
     df_list = tabula.read_pdf(url, pages='all')
     df = pd.DataFrame()
     for dfs in df_list:
-      df = pd.concat([df,dfs])
+      df = pd.concat([df, dfs])
     return df
   
-  def list_number_of_stores(self,headers:dict,endpoint=ENDPOINT):
+  def list_number_of_stores(
+      self,
+      headers:dict,
+      endpoint=ENDPOINT
+      ) -> int | None:
     endpoint = endpoint.format('number_stores')
-    NOS = requests.get(endpoint,headers=headers)
+    NOS = requests.get(endpoint, headers=headers)
     if NOS.status_code < 400:
       self.number_stores = NOS.json()['number_stores']
       self.NOS_endpoint = endpoint
       return NOS.json()['number_stores']
     else:
-      return print(f'Code {NOS.status_code}. Try self.number_stores')
+      return print(f'Code {NOS.status_csode}. Try self.number_stores')
     
-  def retrieve_stores_data(self,headers:dict=headers,endpoint=ENDPOINT):
+  def retrieve_stores_data(
+        self,
+        headers:dict=headers,
+        endpoint=ENDPOINT
+  ) -> pd.DataFrame | None:
     df = pd.DataFrame()
-    NOS = self.list_number_of_stores(headers,endpoint)
+    NOS = self.list_number_of_stores(headers, endpoint)
     if type(NOS) == int:
       for i in range(NOS):
-        RAS = requests.get(endpoint.format(f'store_details/{i}'),headers=headers).json()
-        df = pd.concat([df,pd.DataFrame([RAS])])
+        RAS = requests.get(endpoint.format(f'store_details/{i}'),
+                           headers=headers).json()
+        df = pd.concat([df, pd.DataFrame([RAS])])
       return df
     else:
       print("NOS not an int")
 
-  def extract_from_s3(self,url):
-    bucket,file = url.replace('s3://','').split('/')[0],url.replace('s3://','').split('/')[1:]
+  def extract_from_s3(self, url) -> pd.DataFrame:
+    bucket, file = url.replace('s3://','').split('/')[0],url.replace('s3://','').split('/')[1:]
     s3 = boto3.client('s3')
     response = s3.get_object(Bucket=bucket, Key='/'.join(file))
     data = response['Body'].read()
     return pd.read_csv(BytesIO(data))
-
-if __name__ == '__main__':
-  test = DataExtractor()
-  df = test.read_rds_table('dim_card_details')
-  df2=df.copy()
-  print(df.head())
-  # print(type(df2.join_date[0]))
-  print(df.info())
-  # print(len(df))
-  # print(df.head(5))
-  print(df.columns)
-#   print(df)
--- database_utils.py
+++ database_utils.py
@@ -2,7 +2,6 @@ import pandas as pd
 import yaml
 from sqlalchemy import create_engine, inspect, text
 from sqlalchemy.engine import Engine
-from typing import Union
 
 with open('keys.yaml','r') as f:
   keys = yaml.safe_load(f)
@@ -11,11 +10,9 @@ with open('keys.yaml','r') as f:
 class DatabaseConnector:
 
   def __init__(self):
-    self.current_creds: Union[dict,str] = 'No credentials given.'
-    self.connection: Union[Engine, None]= None
+    self.current_creds: dict | str = 'No credentials given.'
     self.table_list: list = []
-    
-  
+
   def read_db_creds(self, file_path:str='db_creds.yaml')->dict:
     '''
     By default, looks for a `./db_creds.yaml` file.  The following
@@ -26,15 +23,16 @@ class DatabaseConnector:
       self.current_creds = yaml.safe_load(f)
       return self.current_creds
     
-  def init_db_engine(self, file_path:str=self.current_creds) -> Engine:
+  def init_db_engine(self) -> Engine:
     '''
-    The method `read_db_creds()` should be called first.  Previous
-    open connections will be closed.
+    The method `read_db_creds()` should be called first.
     '''
     if type(self.current_creds) == str:
       raise ValueError('Database credentials not supplied; run `read_db_creds()`.')
-    if self.connection is not None:
-      self.close_connection(print_message=False)
+    # if self.engine is not None:
+    #   self.close_connection(print_message=False)
+    
+    file_path = self.current_creds
     creds = self.read_db_creds(file_path)
     conn_str = "postgresql://{}:{}@{}:{}/{}".format(creds['RDS_USER'],
                                                     creds['RDS_PASSWORD'],
@@ -42,10 +40,9 @@ class DatabaseConnector:
                                                     creds['RDS_PORT'],
                                                     creds['RDS_DATABASE']
                                             )
-    self.connection = create_engine(conn_str)
-    return self.connection
+    return create_engine(conn_str)
     
-  def list_db_tables(self, schema:str='public')->list:
+  def list_db_tables(self, schema:str='public') -> list:
     engine = self.init_db_engine()
     self.table_list = inspect(engine).get_table_names(schema=schema)
     return self.table_list
@@ -60,13 +57,3 @@ class DatabaseConnector:
     conn_str = f"postgresql://postgres:{password}@localhost:5432/{db_name}"
     with create_engine(conn_str).connect() as conn:
       df.to_sql(table_name, con=conn, index=False, if_exists='replace')
-
-  def close_connection(self, print_message:bool=True):
-    if self.connection is not None:
-      try:
-        self.connection.close()
-        if print_message:
-          print('Previous connection on {self.current_creds} closed.')
-      except Exception as e:
-        print(f'Error closing the connection on {self.current_creds}: {e}')
-      self.connection = None
